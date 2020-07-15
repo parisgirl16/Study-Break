f0f5edd013c1eb23ecfd93b54f463a9c364925d3
Bella Ott <parisgirl16@sbcglobal.net>
2020-07-15 18:18:14 -0500

    added timer

--- Study Break/TimerViewController.swift
+++ Study Break/TimerViewController.swift
@@ -7,24 +7,53 @@
 //
 
 import UIKit
+import Foundation
 
 class TimerViewController: UIViewController {
 
+    @IBOutlet weak var countdown: UILabel!
+    //declare empty timer variable
+    var timer = Timer()
+    var secondsLeft = 60
+    var minutesLeft = 29
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+            
+    }
+
+    @IBAction func startCountdown(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(startTimer), userInfo: nil, repeats: true)
+        timer.tolerance = 0.3
+        RunLoop.current.add(timer, forMode: RunLoop.Mode.common)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func startTimer() {
+        print("Timer started.")
+        
+        secondsLeft -= 1
+        
+        print(minutesLeft, secondsLeft)
+        
+        if secondsLeft<10 {
+            self.countdown.text = "\(minutesLeft):0\(secondsLeft)"
+        } else {
+            self.countdown.text = "\(minutesLeft):\(secondsLeft)"
+        }
+        
+        
+        if secondsLeft == 0 {
+            minutesLeft-=1
+            secondsLeft = 59
+        }
+        
+        if minutesLeft==0 {
+            timer.invalidate()
+            print("Timer Done")
+            self.countdown.text = "Time's Up!"
+        }
     }
-    */
-
+    
 }
+
